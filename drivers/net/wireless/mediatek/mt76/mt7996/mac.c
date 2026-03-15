9543f9d59f34aaafd6ec86663e75cedd55a5718c
Lorenzo Bianconi <lorenzo@kernel.org>
2026-03-15 11:26:29 +0100

    wifi: mt76: mt7996: Destroy active sta links in mt7996_mac_sta_remove()
    Similar to vif link management, postpone sta link destuction in
    mt7996_mac_sta_remove() introducing mt7996_mac_sta_remove_link utility
    routine and just disable sta link running mt7996_mac_sta_remove_links
    routine.
    This is a preliminary patch in order to support MLO link reconfiguration
    in MT7996 driver.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Link: https://patch.msgid.link/20260315-mt7996-mlo-link-reconf-v1-6-a8a634fbc927@kernel.org
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7996/mac.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/mac.c
@@ -2408,27 +2408,8 @@ mt7996_mac_reset_sta_iter(void *data, struct ieee80211_sta *sta)
 	struct mt7996_dev *dev = data;
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(msta->link); i++) {
-		struct mt7996_sta_link *msta_link = NULL;
-		struct mt7996_phy *phy;
-
-		msta_link = rcu_replace_pointer(msta->link[i], msta_link,
-						lockdep_is_held(&dev->mt76.mutex));
-		if (!msta_link)
-			continue;
-
-		mt7996_mac_sta_deinit_link(dev, msta_link);
-		phy = __mt7996_phy(dev, msta_link->wcid.phy_idx);
-		if (phy)
-			phy->mt76->num_sta--;
-
-		if (msta->deflink_id == i) {
-			msta->deflink_id = IEEE80211_LINK_UNSPECIFIED;
-			continue;
-		}
-
-		kfree_rcu(msta_link, rcu_head);
-	}
+	for (i = 0; i < ARRAY_SIZE(msta->link); i++)
+		mt7996_mac_sta_remove_link(dev, sta, i, true);
 }
 
 static void
